cabbb21c58ca0446323ac144ece4ebf4f6de55bb
LakshmanGamers <lakshmanreddy458@gmail.com>
2023-05-23 16:12:12 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import requests
 
 app = Flask(__name__)
@@ -6,21 +6,25 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def get_news():
-    data = requests.get(
-        "https://newsapi.org/v2/everything?q=Apple&from=2023-05-22&sortBy=popularity&apiKey=913dfba790dc45c58171f4a119b33269")
-    res = data.json()["articles"]
     titles = []
     description = []
     page_url = []
     image_url = []
+    if request.method == "GET":
+        data = requests.get(
+            "https://newsapi.org/v2/everything?q=Apple&from=2023-05-22&sortBy=popularity&apiKey=913dfba790dc45c58171f4a119b33269")
+        res = data.json()["articles"]
+        
 
-    for i in res:
-        titles.append(i["title"])
-        description.append(i["description"])
-        page_url.append(i["url"])
-        image_url.append(i["urlToImage"])
+        for i in res:
+            titles.append(i["title"])
+            description.append(i["description"])
+            page_url.append(i["url"])
+            image_url.append(i["urlToImage"])
 
+        return render_template('index.html', info=[titles, description, page_url, image_url])
     return render_template('index.html', info=[titles, description, page_url, image_url])
+
 
 
 if __name__ == '__main__':
